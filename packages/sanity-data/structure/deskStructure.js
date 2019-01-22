1276d427283279f6ae64d6a8575f7004274d39b9
Siddise Hirpa <siddise.hirpa@knowit.no>
2019-01-22 18:05:47 +0100

    Added mock deskStructure

--- packages/sanity-data/structure/deskStructure.js
+++ packages/sanity-data/structure/deskStructure.js
@@ -5,11 +5,49 @@ export default () =>
     .title("Desk")
     .items([
       S.listItem()
-        .title("Artikkel")
+      .title("Program")
+      .child(
+        S.list()
+        .title("Program")
+        .child(
+          S.listItem()
+            .title("Arrangementer"),
+          S.listItem()
+            .title("Arbeidsliste")
+        ),
+      ),
+      S.listItem()
+        .title("Forsiden")
         .child(
           S.editor()
-            .id('article')
-            .schemaType("article")
-            .documentId("global-config")
-        )
+              .id('frontPage')
+              .schemaType("article")
+              .documentId("global-front-page")),
+      S.listItem()
+        .title("Pride Art"),
+      S.listItem()
+        .title("Pride Park"),
+      S.listItem()
+        .title("Pride House"),
+      S.listItem()
+        .title("Pride Parade"),
+      S.listItem()
+        .title("Om oss"),
+      S.listItem()
+        .title("Kontakt"),
+      S.listItem()
+        .title("Partnere")
     ]);
+
+/* Desk - list
+    frontPage list-item
+    pride-art
+    pride-park
+    ..
+    about
+    contact
+    program
+      events
+      arbeidsliste
+    partners
+*/
