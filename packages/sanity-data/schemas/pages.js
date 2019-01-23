80fae23b2e5265e0522c445cc429f022ace44a6e
Siddise Hirpa <siddise.hirpa@knowit.no>
2019-01-23 19:34:21 +0100

    Fixed formatting

--- packages/sanity-data/schemas/pages.js
+++ packages/sanity-data/schemas/pages.js
@@ -1,10 +1,10 @@
 // Schema to collect all pages in one schema
 
-import frontPage from './frontPage'
+import frontPage from "./frontPage";
 
 export default {
-    name: 'pages',
-    title: 'Pages',
-    type: 'array',
-    of: [frontPage]
-}
+  name: "pages",
+  title: "Pages",
+  type: "array",
+  of: [frontPage]
+};
