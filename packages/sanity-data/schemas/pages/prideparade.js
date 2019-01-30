e8a79563d1b78278e875e77b78122c3b7a7b5f0a
Siddise Hirpa <siddise.hirpa@knowit.no>
2019-01-30 18:02:38 +0100

    Removed unused fields from page schemas

--- packages/sanity-data/schemas/pages/prideparade.js
+++ packages/sanity-data/schemas/pages/prideparade.js
@@ -4,68 +4,24 @@ export default {
   type: "document",
   fields: [
     {
-      name: "title",
-      title: "Tittel",
-      type: "string",
-      validation: Rule => Rule.required()
-    },
-    {
-      name: "slug",
-      title: "Adresse",
-      type: "slug",
-      options: {
-        source: "title",
-        maxLength: 200, // will be ignored if slugify is set
-        slugify: input =>
-          input
-            .toLowerCase()
-            .replace(/\s+/g, "-")
-            .slice(0, 200)
-      }
-    },
-    {
-      name: "image",
-      title: "Hovedbilde",
-      type: "image",
-      options: {
-        hotspot: true
-      }
-    },
-    {
       name: "preamble",
       title: "Ingress",
       type: "array",
       of: [{ type: "block" }]
     },
     {
+      name: "image",
+      title: "Bilde",
+      type: "image",
+      options: {
+        hotspot: true
+      }
+    },
+    {
       name: "body",
-      title: "Brødtekst",
+      title: "Innhold",
       type: "array",
       of: [{ type: "block" }]
-    },
-    {
-      name: "weight",
-      title: "Vekting",
-      type: "string",
-      options: {
-        list: [
-          { title: "Uviktig", value: "1" },
-          { title: "Normal", value: "5" },
-          { title: "Fremhevet", value: "10" }
-        ]
-      }
-    },
-    {
-      name: "editorialState",
-      type: "string",
-      options: {
-        list: [
-          { title: "Needs review", value: "review" },
-          { title: "Awaiting publication", value: "awaiting" },
-          { title: "Published", value: "published" }
-        ],
-        layout: "radio"
-      }
     }
   ]
 };
