16dda5309390ef693f29c4c2f196a0f430b2c083
Siddise Hirpa <siddisehirpa@gmail.com>
2019-03-12 18:56:52 +0100

    Fix format

--- packages/sanity-data/schemas/blockContent.js
+++ packages/sanity-data/schemas/blockContent.js
@@ -1,32 +1,35 @@
 export default {
-  title: 'Block Content',
-  name: 'blockContent',
-  type: 'array',
+  title: "Block Content",
+  name: "blockContent",
+  type: "array",
   of: [
     {
-      title: 'Block',
-      type: 'block',
+      title: "Block",
+      type: "block",
       styles: [
-        {title: 'Normal', value: 'normal'},
-        {title: 'H1', value: 'h1'},
-        {title: 'H2', value: 'h2'},
-        {title: 'H3', value: 'h3'},
-        {title: 'H4', value: 'h4'},
-        {title: 'Quote', value: 'blockquote'}
+        { title: "Normal", value: "normal" },
+        { title: "H1", value: "h1" },
+        { title: "H2", value: "h2" },
+        { title: "H3", value: "h3" },
+        { title: "H4", value: "h4" },
+        { title: "Quote", value: "blockquote" }
       ],
-      lists: [{title: 'Bullet', value: 'bullet'}],
+      lists: [{ title: "Bullet", value: "bullet" }],
       marks: {
-        decorators: [{title: 'Strong', value: 'strong'}, {title: 'Emphasis', value: 'em'}],
+        decorators: [
+          { title: "Strong", value: "strong" },
+          { title: "Emphasis", value: "em" }
+        ],
         annotations: [
           {
-            title: 'URL',
-            name: 'link',
-            type: 'object',
+            title: "URL",
+            name: "link",
+            type: "object",
             fields: [
               {
-                title: 'URL',
-                name: 'href',
-                type: 'url'
+                title: "URL",
+                name: "href",
+                type: "url"
               }
             ]
           }
@@ -35,8 +38,8 @@ export default {
     },
 
     {
-      type: 'image',
-      options: {hotspot: true}
+      type: "image",
+      options: { hotspot: true }
     }
   ]
-}
+};
