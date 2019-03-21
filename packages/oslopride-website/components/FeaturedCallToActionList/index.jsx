7cc95085943b1a2f010306688e5a1daa3775566f
Boye Borg <boye.borg@icloud.com>
2019-03-21 20:19:56 +0100

    add call to action icons (#63)

--- packages/oslopride-website/components/FeaturedCallToActionList/index.jsx
+++ packages/oslopride-website/components/FeaturedCallToActionList/index.jsx
@@ -1,3 +1,4 @@
+import { imageUrlFor } from "@/store/sanity";
 import React from "react";
 import styled from "styled-components";
 import Button from "../Button";
@@ -15,16 +16,38 @@ const Wrapper = styled.ul`
 const CallToActionButton = styled(Button)`
   font-weight: bold;
   width: 100%;
-  padding: 20px 15px;
+  padding: 10px 15px;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: inherit;
+`;
+
+const CallToActionImage = styled.img`
+  height: 45px;
+  width: auto;
+  margin-right: 20px;
+`;
+
+const CallToActionText = styled.span`
+  text-align: center;
+  flex-grow: 1;
+  text-decoration: none;
 `;
 
 const FeaturedCallToActionList = ({ className, featuredCallToActions }) => (
   <Wrapper className={className}>
-    {featuredCallToActions.map(({ _key: key, link, title }) => (
+    {featuredCallToActions.map(({ _key: key, link, title, icon }) => (
       <li key={key}>
-        <a href={link}>
-          <CallToActionButton>{title}</CallToActionButton>
-        </a>
+        <CallToActionButton as="a" href={link}>
+          <CallToActionImage
+            src={imageUrlFor(icon)
+              .width(50)
+              .url()}
+            alt={title}
+          />
+          <CallToActionText>{title}</CallToActionText>
+        </CallToActionButton>
       </li>
     ))}
   </Wrapper>
