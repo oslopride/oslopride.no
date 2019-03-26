874671453611c010cff8ce69ee97a9ce78c159d1
Boye Borg <boye.borg@icloud.com>
2019-03-26 08:49:26 +0100

    Simplify deploy script (#75)
    * build on version tags
    
    * only use one deploy script
    
    * update deploy script to cover all cases
    
    * fix typo
    
    * add empty line at end of file
    
    * fix tag regex if

--- packages/oslopride-website/scripts/alias-now.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-	now alias set $NOW_DEPLOYMENT beta.oslopride.no --token=$NOW_TOKEN --scope oslopride;
-fi
--- packages/oslopride-website/scripts/deploy-now.sh
+++ packages/oslopride-website/scripts/deploy-now.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-export NOW_DEPLOYMENT=$(now --token=$NOW_TOKEN --team oslopride --no-clipboard)
+# Deploy
+NOW_DEPLOYMENT=$(now --token=$NOW_TOKEN --scope oslopride --no-clipboard)
+
+# Pull Request
+if [ $TRAVIS_PULL_REQUEST != "false" ]; then
+	curl -H "Authorization: token $PRIDEBOT_TOKEN" -X POST -d "{\"body\": \"${NOW_DEPLOYMENT}\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments";
+
+# Commit to master
+elif [ $TRAVIS_BRANCH = "master" ]; then
+  now alias set $NOW_DEPLOYMENT beta.oslopride.no --token=$NOW_TOKEN --scope oslopride;
+
+# Release
+elif [[ $TRAVIS_TAG =~ ^v([0-9]+)\.([0-9]+)\.([0-9]+)$ ]] && [ $TRAVIS_BRANCH = $TRAVIS_TAG ]; then
+	now alias set $NOW_DEPLOYMENT oslopride.no --token=$NOW_TOKEN --scope oslopride;
+fi
