9189a2beaaaf0d0c98522898b9a4aea20cb1b933
Boye Borg <boye.borg@icloud.com>
2019-04-24 08:50:39 +0200

    update deploy procedure (#103)

--- packages/oslopride-website/scripts/deploy-now.sh
+++ packages/oslopride-website/scripts/deploy-now.sh
@@ -6,12 +6,7 @@ NOW_DEPLOYMENT=$(now --token=$NOW_TOKEN --scope oslopride --no-clipboard)
 # Pull Request
 if [ $TRAVIS_PULL_REQUEST != "false" ]; then
 	curl -H "Authorization: token $PRIDEBOT_TOKEN" -X POST -d "{\"body\": \"${NOW_DEPLOYMENT}\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments";
-
-# Commit to master
+# Release on commit to master
 elif [ $TRAVIS_BRANCH = "master" ]; then
-  now alias set $NOW_DEPLOYMENT beta.oslopride.no --token=$NOW_TOKEN --scope oslopride;
-
-# Release
-elif [[ $TRAVIS_TAG =~ ^v([0-9]+)\.([0-9]+)\.([0-9]+)$ ]] && [ $TRAVIS_BRANCH = $TRAVIS_TAG ]; then
-	now alias set $NOW_DEPLOYMENT oslopride.no --token=$NOW_TOKEN --scope oslopride;
+  now alias set $NOW_DEPLOYMENT www.oslopride.no --token=$NOW_TOKEN --scope oslopride;
 fi
