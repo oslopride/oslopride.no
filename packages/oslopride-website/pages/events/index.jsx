0e14035178748d3bbf7c61ef1921ce8809649d56
Siddise Hirpa <siddisehirpa@gmail.com>
2019-05-22 00:02:29 +0200

    show venue on eventlist (#129)
    * change title of event day style
    
    * Added styling for image and font of eventlist
    
    * change color and positioning of eventlist
    
    * center image and text of event on list
    
    * add placeholder image

--- packages/oslopride-website/pages/events/index.jsx
+++ packages/oslopride-website/pages/events/index.jsx
@@ -1,6 +1,7 @@
 import Sheet from "@/components/Sheet";
 import { eventsActions, getEvents } from "@/store/events";
 import { webResponseInitial } from "@/store/helpers";
+import { imageUrlFor } from "@/store/sanity";
 import theme from "@/utils/theme";
 import dayjs from "dayjs";
 import NextSeo from "next-seo";
@@ -49,46 +50,63 @@ const Events = props => {
 
   return (
     <>
-      <PageTitle>Program 2019</PageTitle>
+      <Sheet>
+        <PageTitle>Program 2019</PageTitle>
 
-      {!events.data.length ? (
-        <Sheet>
-          <p>Kommer snart!</p>
-        </Sheet>
-      ) : null}
+        {!events.data.length ? <p>Kommer snart!</p> : null}
 
-      {groupEventsByDay(events.data).map(day => {
-        const currentDay = dayjs(day[0].startingTime);
-        return (
-          <Event key={currentDay.format("YYYY-MM-DD")}>
-            <div>
+        {groupEventsByDay(events.data).map(day => {
+          const currentDay = dayjs(day[0].startingTime);
+          return (
+            <Event key={currentDay.format("YYYY-MM-DD")}>
               <EventDay>
-                {currentDay.format("dddd")}{" "}
-                <EventDate>{currentDay.format("D. MMMM")}</EventDate>
+                <h2>
+                  {currentDay.format("dddd")}{" "}
+                  <span>{currentDay.format("D. MMMM")}</span>
+                </h2>
               </EventDay>
-            </div>
-            <div>
-              {day.map(event => (
-                <Link
-                  key={event._id}
-                  href={`/event?id=${event._id}`}
-                  as={`/events/${event._id}`}
-                >
-                  <EventLink>
-                    <a>
-                      <EventTime>
-                        {dayjs(event.startingTime).format("HH:mm")}-
-                        {dayjs(event.endingTime).format("HH:mm")}
-                      </EventTime>
-                      <EventTitle>{event.title}</EventTitle>
-                    </a>
-                  </EventLink>
-                </Link>
-              ))}
-            </div>
-          </Event>
-        );
-      })}
+              <div>
+                {day.map(event => (
+                  <Link
+                    key={event._id}
+                    href={`/event?id=${event._id}`}
+                    as={`/events/${event._id}`}
+                  >
+                    <EventLink>
+                      <a>
+                        {event.image ? (
+                          <EventImage
+                            src={imageUrlFor(event.image)
+                              .height(250)
+                              .url()}
+                            alt="arrangementsbilde"
+                          />
+                        ) : (
+                          <EventImage
+                            src="static/placeholder.jpg"
+                            alt="arrangementsbilde"
+                          />
+                        )}
+
+                        <EventInfo>
+                          <EventTitle>{event.title}</EventTitle>
+                          <EventTime>
+                            {dayjs(event.startingTime).format("HH:mm")}-
+                            {dayjs(event.endingTime).format("HH:mm")}
+                          </EventTime>
+                          <EventPlace>
+                            {event.location.name}, {event.location.address}
+                          </EventPlace>
+                        </EventInfo>
+                      </a>
+                    </EventLink>
+                  </Link>
+                ))}
+              </div>
+            </Event>
+          );
+        })}
+      </Sheet>
 
       <NextSeo
         config={{
@@ -137,43 +155,76 @@ export default connect(mapStateToProps)(Events);
 const PageTitle = styled.h1`
   color: ${theme.purple};
   text-transform: uppercase;
+  text-align: center;
 `;
 
-const Event = styled(Sheet)`
+const Event = styled.div`
   width: 100%;
   max-width: 1000px;
-  margin: 20px;
 `;
 
-const EventDay = styled.h2`
-  font-size: 25px;
-  color: ${theme.orange};
-  text-transform: uppercase;
-`;
+const EventDay = styled.div`
+  background-color: ${theme.purple};
+  width: 100%;
 
-const EventDate = styled.span`
-  text-transform: uppercase;
-  color: initial;
+  h2 {
+    font-size: 25px;
+    font-weight: 500;
+    color: white;
+    text-transform: uppercase;
+    text-align: center;
+  }
 `;
 
 const EventLink = styled.div`
   cursor: pointer;
   border-bottom: 2px solid lightgrey;
   padding: 10px 0;
+
   &:last-child {
     border-bottom: 0;
   }
 
   & > a {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
     text-decoration: none;
+
+    :hover {
+      text-decoration: underline;
+    }
   }
 `;
 
-const EventTime = styled.div`
-  font-weight: bold;
-  margin-bottom: 5px;
+const EventImage = styled.img`
+  width: 80px;
+  height: 80px;
+  object-fit: cover;
+`;
+
+const EventInfo = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-start;
+  margin-left: 20px;
+  width: 100%;
 `;
 
 const EventTitle = styled.div`
-  text-decoration: underline;
+  width: 100%;
+  font-size: 20px;
+  font-weight: 500;
+`;
+
+const EventTime = styled.div`
+  font-size: 18px;
+  font-weight: 600;
+  color: ${theme.orange};
+  margin-right: 10px;
+`;
+
+const EventPlace = styled.div`
+  font-size: 18px;
+  font-weight: 300;
 `;
--- packages/oslopride-website/utils/theme.js
+++ packages/oslopride-website/utils/theme.js
@@ -2,12 +2,13 @@ import { rgb } from "polished";
 
 export default {
   red: rgb(164, 29, 47),
-  orange: rgb(239, 94, 44),
+  orange: rgb(239, 82, 27),
   yellow: rgb(255, 193, 1),
   green: rgb(65, 140, 98),
   blue: rgb(51, 80, 185),
   lightBlue: rgb(167, 195, 245),
   purple: rgb(53, 32, 118),
+  lightPurple: rgb(191, 180, 211),
   pink: rgb(227, 80, 161),
   gray: rgb(204, 204, 204),
   darkgray: rgb(74, 74, 74),
