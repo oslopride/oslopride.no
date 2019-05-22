da56f2010cc8f2f5e9c1c9957c2b3d13ffc2477e
Christian Knappskog <christian@knappskog.priv.no>
2019-05-22 23:06:11 +0200

    Revert "add link to events (#125)"
    This reverts commit 7e07c41ed5772c9c80cf4e7c7bbfd4ea2a5f39f5.

--- packages/oslopride-website/components/Header/navigation.jsx
+++ packages/oslopride-website/components/Header/navigation.jsx
@@ -30,15 +30,6 @@ const Navigation = ({ className, visible, callback }) => (
   <Container className={className} visible={visible}>
     <NavigationGroup>
       <NavigationLink
-        href="/events"
-        onClick={callback}
-        color={lighten(0.1, theme.yellow)}
-      >
-        Program 2019
-      </NavigationLink>
-    </NavigationGroup>
-    <NavigationGroup>
-      <NavigationLink
         href="/pride-parade"
         onClick={callback}
         color={lighten(0.3, theme.red)}
