9fe1e72f158e3af6dcc030b496a83b97682719da
Boye Borg <boye.borg@icloud.com>
2020-01-11 19:44:33 +0100

    initialize gatsby web project

---
+++ packages/web/src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function IndexPage() {
+  return (
+    <Layout>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+    </Layout>
+  );
+}
