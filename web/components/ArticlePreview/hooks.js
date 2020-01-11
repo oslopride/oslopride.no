6b1a2d07417bf69045c92a3f98a09d0831a7ffa1
Boye Borg <boye.borg@icloud.com>
2020-01-11 09:08:43 +0100

    clean house

--- web/components/ArticlePreview/hooks.js
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-
-export const useFetchArticleIfNotPresent = (articlePresent, fetchArticle) =>
-  useEffect(() => {
-    if (!articlePresent) {
-      fetchArticle();
-    }
-  }, []);
