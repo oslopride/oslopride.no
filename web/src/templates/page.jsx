1b783d656218d7b7c30598c3b302d9842bc395c9
Boye Borg <boye.borg@icloud.com>
2020-01-12 13:46:46 +0100

    support image block

--- web/src/templates/page.jsx
+++ web/src/templates/page.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
 import Hero from "../blocks/hero";
+import Image from "../blocks/image";
 
 export default function Page({ data: { page }, pageContext: { locale } }) {
 	const blocks = (page._rawBlocks[locale] || []).map(block => {
 		switch (block._type) {
 			case "hero":
 				return <Hero key={block._key} {...block} />;
+			case "image":
+				return <Image key={block._key} image={block} />;
 			default:
 				console.warn(`Unknown block type: "${block._type}"`);
 				if (process.env.NODE_ENV !== "production") {
