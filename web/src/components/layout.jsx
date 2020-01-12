15ee85a148885b1c69a002d64ce92b5d28b2c1de
Boye Borg <boye.borg@icloud.com>
2020-01-12 14:24:48 +0100

    show header

--- web/src/components/layout.jsx
+++ web/src/components/layout.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { Link } from "gatsby";
+import logo from "../assets/logo.svg";
+import Navigation from "./navigation";
 
-export default function Layout({ children }) {
-	return <div>{children}</div>;
+export default function Layout({ children, locale }) {
+	const baseUrl = locale === "no" ? "/" : `/${locale}/`;
+
+	return (
+		<>
+			<header>
+				<Link to={baseUrl}>
+					<img width="100px" src={logo} alt="Oslo Pride logo" />
+				</Link>
+				<Navigation locale={locale} />
+			</header>
+			{children}
+			<footer>OSLO PRIDE</footer>
+		</>
+	);
 }
