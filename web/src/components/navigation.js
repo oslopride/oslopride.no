64e4793c733e9a8780fddaab66bfc2c540e3d860
Boye Borg <boye.borg@icloud.com>
2020-01-18 14:26:05 +0100

    add styling to navbar

--- web/src/components/navigation.js
+++ web/src/components/navigation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, graphql, useStaticQuery } from "gatsby";
+import styled from "styled-components";
 
 export default function({ locale }) {
 	const data = useStaticQuery(
@@ -19,21 +20,63 @@ export default function({ locale }) {
 			}
 		`
 	);
+	const [navigationVisible, showNavigation] = React.useState(false);
+	const toggleNavigation = () => showNavigation(current => !current);
+	const closeNavigation = () => showNavigation(false);
 
 	const prefix = locale === "no" ? "/" : `/${locale}/`;
 
-	const links = (data.sanityConfiguration.navigation_bar || []).map(page => (
-		<li key={page.slug.current}>
-			{page.title[locale] && (
-				<Link
-					to={`${prefix}${page.slug.current}`}
-					aria-label={page.title[locale]}
-				>
-					{page.title[locale]}
-				</Link>
-			)}
-		</li>
-	));
+	const links = (data.sanityConfiguration.navigation_bar || []).map(
+		page =>
+			page.title[locale] && (
+				<NavigationItem key={page.slug.current} onClick={closeNavigation}>
+					<Link
+						to={`${prefix}${page.slug.current}`}
+						aria-label={page.title[locale]}
+					>
+						{page.title[locale]}
+					</Link>
+				</NavigationItem>
+			)
+	);
 
-	return <ul>{links}</ul>;
+	return (
+		<>
+			<NavigationToggleButton onClick={toggleNavigation}>
+				{navigationVisible ? "close" : "menu"}
+			</NavigationToggleButton>
+			<Navigation visible={navigationVisible}>
+				<NavigationList>{links}</NavigationList>
+			</Navigation>
+		</>
+	);
 }
+
+const Navigation = styled.div`
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100vw;
+	height: 100vh;
+	background: white;
+	display: ${props => (props.visible ? "flex" : "none")};
+	flex-direction: column;
+	align-items: center;
+	padding: 5rem 2rem;
+`;
+
+const NavigationList = styled.ul`
+	list-style: none;
+	margin: 0;
+	padding: 0;
+	font-size: 2em;
+`;
+
+const NavigationItem = styled.li``;
+
+const NavigationToggleButton = styled.button`
+	position: absolute;
+	top: 20px;
+	right: 20px;
+	z-index: 1;
+`;
