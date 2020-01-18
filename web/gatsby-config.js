1e799afd3006ec3d07af4bc030e482e253fd99c1
Boye Borg <boye.borg@icloud.com>
2020-01-18 15:00:04 +0100

    Add styling (#16)
    * add styled-components
    
    * add simple styling to hero
    
    * add font and global some global defaults
    
    * improve styling
    
    * add styling to navbar

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,10 +1,19 @@
 module.exports = {
 	plugins: [
+		"gatsby-plugin-styled-components",
 		{
 			resolve: "gatsby-source-sanity",
 			options: {
 				projectId: "2ger3rla",
 				dataset: "future"
+			}
+		},
+		{
+			resolve: "gatsby-plugin-web-font-loader",
+			options: {
+				typekit: {
+					id: "ygk6hzk"
+				}
 			}
 		}
 	]
--- web/src/blocks/hero.js
+++ web/src/blocks/hero.js
@@ -1,31 +1,92 @@
 import React from "react";
+import styled from "styled-components";
 import Anchor from "../components/anchor";
 import { urlFor } from "../utils/sanity";
 
 export default function Hero({ title, subtitle, links, image }) {
+	const imageRef = React.useRef(null);
+	const [imageHeight, setImageHeight] = React.useState(0);
+	const updateImageHeight = () =>
+		setImageHeight((imageRef.current || {}).offsetHeight || 0);
+
 	return (
-		<div>
-			<h1>{title}</h1>
+		<Wrapper>
+			<ImageContainer>
+				{image && (
+					<>
+						<Image
+							onLoad={updateImageHeight}
+							ref={imageRef}
+							src={urlFor(image)
+								.maxWidth(500)
+								.url()}
+						/>
+					</>
+				)}
+			</ImageContainer>
 
-			{subtitle && <h2>{subtitle}</h2>}
+			<TextContainer imageHeight={imageHeight}>
+				<h1>{title}</h1>
 
-			{links && links.lenght > 0 && (
-				<ul>
-					{links.map(link => (
-						<li key={link._key}>
-							<Anchor {...link} />
-						</li>
-					))}
-				</ul>
-			)}
+				{subtitle && <p>{subtitle}</p>}
 
-			{image && (
-				<img
-					src={urlFor(image)
-						.maxWidth(500)
-						.url()}
-				/>
-			)}
-		</div>
+				{links && links.lenght > 0 && (
+					<ul>
+						{links.map(link => (
+							<li key={link._key}>
+								<Anchor {...link} />
+							</li>
+						))}
+					</ul>
+				)}
+			</TextContainer>
+		</Wrapper>
 	);
 }
+
+const Wrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	max-width: 1000px;
+	margin: 0 auto;
+	padding: 0 1rem;
+
+	@media (min-width: 700px) {
+		flex-direction: row-reverse;
+	}
+`;
+
+const ImageContainer = styled.div`
+	flex: 1;
+	position: relative;
+	z-index: -1;
+
+	&::after {
+		content: "";
+		position: absolute;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		background: linear-gradient(to top, white, transparent);
+	}
+
+	@media (min-width: 700px) {
+		&::after {
+			display: none;
+		}
+	}
+`;
+
+const Image = styled.img`
+	max-width: 100%;
+`;
+
+const TextContainer = styled.div`
+	flex: 1;
+	margin-top: -${props => props.imageHeight * 0.35}px;
+
+	@media (min-width: 700px) {
+		margin-top: 0;
+	}
+`;
