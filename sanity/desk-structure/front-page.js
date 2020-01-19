193e0d94f859ff9b391ffe5efa156a7a7635ec6f
Boye Borg <boye.borg@icloud.com>
2020-01-19 12:56:43 +0100

    add page preview to front page

--- sanity/desk-structure/front-page.js
+++ sanity/desk-structure/front-page.js
@@ -3,6 +3,7 @@ import EditIcon from "part:@sanity/base/edit-icon";
 import { MdHome } from "react-icons/lib/md";
 
 import JSONpreview from "./previews/json-preview";
+import PagePreview from "./previews/page-preview";
 
 export default S.listItem()
 	.title("Front Page")
@@ -15,6 +16,7 @@ export default S.listItem()
 			.documentId("global_front_page")
 			.views([
 				S.view.form().icon(EditIcon),
+				S.view.component(PagePreview).title("Web"),
 				S.view.component(JSONpreview).title("JSON")
 			])
 	);
