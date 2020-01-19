c86f6d4bc0446afec412d0b1be08a32d08b338c2
Boye Borg <boye.borg@icloud.com>
2020-01-19 12:53:12 +0100

    remove unused code

--- web/src/components/link.js
+++ web/src/components/link.js
@@ -3,24 +3,7 @@ import { usePageContext } from "../hooks/page-context";
 import GatsbyLink from "gatsby-link";
 
 export default function Link({ to, children, ...rest }) {
-	// const [LinkComponent, setLinkComponent] = React.useState("a");
 	const { gatsbyEnvironment } = usePageContext();
-
-	// React.useEffect(() => {
-	// 	if (gatsbyEnvironment) {
-	// 		import("gatsby-link").then(({ Link }) => {
-	// 			setLinkComponent(Link);
-	// 		});
-	// 	}
-	// }, [gatsbyEnvironment]);
-
-	// let props = {};
-
-	// if (LinkComponent !== "a") {
-	// 	props = { to: href, ...rest };
-	// } else {
-	// 	props = { href, ...rest };
-	//
 
 	if (gatsbyEnvironment) {
 		return (
