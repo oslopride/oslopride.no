3bc17151c82b27b1d2bd74f2e93ddb341678afa3
Boye Borg <boye.borg@icloud.com>
2020-02-22 12:33:15 +0100

    restructure project

---
+++ web/webpack.config.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+const webpack = require("webpack");
+const path = require("path");
+
+const config = {
+	entry: ["react-hot-loader/patch", "./src/index.tsx"],
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				use: "babel-loader",
+				exclude: /node_modules/
+			},
+			{
+				test: /\.ts(x)?$/,
+				use: ["awesome-typescript-loader"],
+				exclude: /node_modules/
+			}
+		]
+	},
+	resolve: {
+		extensions: [".js", ".jsx", ".tsx", ".ts"],
+		alias: {
+			"react-dom": "@hot-loader/react-dom"
+		}
+	},
+	devServer: {
+		contentBase: "./dist"
+	}
+};
+
+module.exports = config;
