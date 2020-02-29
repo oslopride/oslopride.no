b826e461ab94b3e584e5233732e9cfe071c8ed5b
Boye Borg <boye.borg@icloud.com>
2020-02-29 10:06:37 +0100

    improve project setup

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-const webpack = require("webpack");
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const path = require("path");
 
 const config = {
@@ -8,29 +8,30 @@ const config = {
 		path: path.resolve(__dirname, "public"),
 		filename: "bundle.js"
 	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				use: "babel-loader",
-				exclude: /node_modules/
-			},
-			{
-				test: /\.ts(x)?$/,
-				use: ["awesome-typescript-loader"],
-				exclude: /node_modules/
-			}
-		]
-	},
 	resolve: {
 		extensions: [".js", ".jsx", ".tsx", ".ts"],
 		alias: {
 			"react-dom": "@hot-loader/react-dom"
 		}
 	},
+	module: {
+		rules: [
+			{
+				test: /\.(ts|js)x?$/,
+				exclude: /node_modules/,
+				use: "babel-loader"
+			},
+			{
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+		]
+	},
 	devServer: {
-		contentBase: "./public"
-	}
+		contentBase: path.join(__dirname, "public"),
+		historyApiFallback: true
+	},
+	plugins: [new ForkTsCheckerWebpackPlugin({ eslint: true })]
 };
 
 module.exports = config;
