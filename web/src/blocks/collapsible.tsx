63bc3bd4a8c7e80a8c85f68e12008c4a3be27a5f
Roger Andersen <hexjelly@hexjelly.com>
2020-03-06 02:32:58 +0100

    add collapsible functionality

--- web/src/blocks/collapsible.tsx
+++ web/src/blocks/collapsible.tsx
@@ -1,25 +1,66 @@
-import React from "react";
-import { SanityCollapsibleList } from "../sanity/models";
+import React, { useState } from "react";
 import BlockContentToReact from "@sanity/block-content-to-react";
+import { css } from "@emotion/core";
+
+import { SanityCollapsibleList } from "../sanity/models";
 
 type Props = {
 	content: SanityCollapsibleList;
 };
 
-const CollapsibleList: React.FC<Props> = ({ content }) => {
-	const { title, listItems } = content;
+const CollapsibleList: React.FC<Props> = ({
+	content: { title, listItems }
+}) => {
+	const [active, setActive] = useState<number | null>(null);
+
+	const onClickHandler = (index: number): void => {
+		setActive(index === active ? null : index);
+	};
+
 	return (
-		<>
+		<div
+			css={css`
+				padding: 15px;
+			`}
+		>
 			<h2>{title}</h2>
-			<ul>
-				{listItems.map((item, idx) => (
-					<li key={`${idx}-${item.title}`}>
-						<h3>{item.title}</h3>
-						<BlockContentToReact blocks={item.content} />
-					</li>
-				))}
+			<ul
+				css={css`
+					list-style: none;
+					padding: 0;
+				`}
+			>
+				{listItems.map((item, idx) => {
+					const isActive = active === idx;
+					return (
+						<li key={`${idx}-${item.title}`}>
+							<h3
+								css={css`
+									display: flex;
+									justify-content: space-between;
+									background-color: ${isActive ? "green" : "lightgreen"};
+									color: ${isActive ? "white" : "black"};
+									padding: 15px;
+									margin: 5px 0;
+								`}
+								onClick={() => onClickHandler(idx)}
+							>
+								<span>{item.title}</span>
+								<span>{isActive ? "-" : "+"}</span>
+							</h3>
+							<div
+								css={css`
+									display: ${isActive ? "block" : "none"};
+									padding: 3px 15px;
+								`}
+							>
+								<BlockContentToReact blocks={item.content} />
+							</div>
+						</li>
+					);
+				})}
 			</ul>
-		</>
+		</div>
 	);
 };
 
