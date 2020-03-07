b6d97a435276fba5571757ffd3866dcf0082eff4
Roger Andersen <hexjelly@hexjelly.com>
2020-03-07 16:01:37 +0100

    add option to import SVG as component

--- web/src/types/svg.d.ts
+++ web/src/types/svg.d.ts
@@ -1,4 +1,5 @@
 declare module "*.svg" {
+	export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
 	const content: React.FC<React.HTMLProps<SVGElement>>;
 	export default content;
 }
