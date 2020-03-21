1f566509b120749d75e7f1dec3a4a8a34de423e2
Sarah Van Neyghem <hello@sarahvn.io>
2020-03-21 13:34:06 +0100

    add hero to article page

--- web/src/pages/article.tsx
+++ web/src/pages/article.tsx
@@ -2,8 +2,27 @@ import React from "react";
 import { RouteComponentProps } from "@reach/router";
 import useSWR from "swr";
 import { SanityPage } from "../sanity/models";
+import Hero from "../components/hero";
+import theme from "../utils/theme";
+import { urlFor } from "../sanity";
+import { css } from "@emotion/core";
 
 type Props = { slug?: string } & RouteComponentProps;
+
+const hero = css`
+	color: #ffffff;
+	text-align: center;
+
+	h2 {
+		font-size: 2.5rem;
+		margin: 0 0 2rem 0;
+	}
+
+	p {
+		font-size: 1rem;
+		margin: 0;
+	}
+`;
 
 const Page: React.FC<Props> = props => {
 	const { slug } = props;
@@ -12,9 +31,25 @@ const Page: React.FC<Props> = props => {
 		`*[_type == "article" && slug.current == "${slug}"] | order(_updatedAt desc) [0]`
 	);
 
+	console.log("PAGE", page);
+
 	return (
 		<>
-			<h2>Article</h2>
+			<Hero
+				angleDirection="<"
+				anglePosition="after"
+				height="50vh"
+				color={theme.color.main.purple}
+				imageUrl={
+					urlFor(page.image)
+						.width(window.innerWidth)
+						.url() || ""
+				}
+				css={hero}
+			>
+				<h2>{page.title.no}</h2>
+				<p>{page.title.no}</p>
+			</Hero>
 		</>
 	);
 };
