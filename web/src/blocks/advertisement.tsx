f18e6a6111e0894c7945a39d7cb48eaea5d421e0
Roger Andersen <hexjelly@hexjelly.com>
2020-05-26 18:49:19 +0200

    fix SubHeading prop from master changes

--- web/src/blocks/advertisement.tsx
+++ web/src/blocks/advertisement.tsx
@@ -96,7 +96,7 @@ const Advertisement: React.FC<Props> = props => {
 	return (
 		<article css={advertisementStyle()}>
 			<header css={headerStyle()}>
-				<SubHeading>{content.category}</SubHeading>
+				<SubHeading line="left">{content.category}</SubHeading>
 				<h2>{content.title}</h2>
 				<BlockContentToReact blocks={content.content} />
 				<ul>
