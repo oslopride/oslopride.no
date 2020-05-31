415213e492e1f81fcc32f51ff6ab31bea2146b0f
Boye Borg <6630430+boyeborg@users.noreply.github.com>
2020-05-31 22:44:25 +0200

    Netlify footer link (#116)
    * remove redundant redirect
    
    * add netlify footer link

--- web/src/components/footer/styles.ts
+++ web/src/components/footer/styles.ts
@@ -10,7 +10,7 @@ export const StickyFooter = styled.footer`
 export const Footer = styled.div`
 	width: 90vw;
 	max-width: 900px;
-	margin: 2.75rem auto;
+	margin: 2.75rem auto 0 auto;
 
 	display: -ms-grid;
 	-ms-grid-columns: 3fr 2fr 1fr;
@@ -19,7 +19,7 @@ export const Footer = styled.div`
 	grid-template-columns: 3fr 2fr 1fr;
 	column-gap: 1rem;
 
-	padding: 2.5rem 0;
+	padding: 2.5rem 1rem;
 
 	a {
 		color: ${theme.color.background.pink};
