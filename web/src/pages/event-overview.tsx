d9889f4ae67b0c9c9e889edb018c3cc03658f57b
Christian Knappskog <christiankn@users.noreply.github.com>
2020-06-01 11:36:43 +0200

    Cut off long event descriptions (#118)
    * Cut off long event descriptions
    Ensure long description-texts on events does not break layout.
    
    * Use LinkButton for event
    * Open external links in new tab

--- web/src/pages/event-overview.tsx
+++ web/src/pages/event-overview.tsx
@@ -11,6 +11,7 @@ import BlockContentToReact from "@sanity/block-content-to-react";
 import Loading from "../components/loading";
 import NotFound from "./not-found";
 import Error from "./error";
+import { LinkButton } from "../components/link";
 
 type Props = { slug?: string } & RouteComponentProps;
 
@@ -133,23 +134,12 @@ const date = css`
 const eventLink = css`
 	margin: 1.5rem;
 	width: calc(100% - 3rem);
-	display: block;
-	background-color: ${theme.color.main.blue};
-	text-transform: uppercase;
-	letter-spacing: 2px;
-	padding: 1em 1.7em;
 	margin-top: 1rem;
-	text-decoration: none;
-	text-align: center;
-	cursor: pointer;
-	border-radius: 4px;
-	color: #ffffff;
-	font-weight: bold;
+`;
 
-	:hover {
-		color: #ffffff;
-		background-color: ${theme.color.main.purple};
-	}
+const descriptionContainer = css`
+	max-height: 200px;
+	overflow-y: hidden;
 `;
 
 const groupEventsByDay = (events: SanitySimpleEventList) => {
@@ -271,11 +261,18 @@ const EventOverview: React.FC<Props> = () => {
 												</time>
 												{event.price && " Pris: " + event.price}
 											</div>
-											<BlockContentToReact blocks={event.description.no} />
+											<div css={descriptionContainer}>
+												<BlockContentToReact blocks={event.description.no} />
+											</div>
 										</div>
-										<a css={eventLink} href={event.eventLink}>
-											Gå til event
-										</a>
+										<LinkButton
+											css={eventLink}
+											link={{
+												_type: "externalLink",
+												text: "Gå til event",
+												url: event.eventLink
+											}}
+										/>
 									</article>
 								))}
 							</div>
