a8df3ee34f687a307c142a874bf5963bd623f415
Boye Borg <boye.borg@icloud.com>
2020-06-16 20:08:06 +0200

    add sourcemaps

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { EnvironmentPlugin } = require("webpack");
+const { EnvironmentPlugin, SourceMapDevToolPlugin } = require("webpack");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 const SentryWebpackPlugin = require("@sentry/webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
@@ -70,11 +70,15 @@ module.exports = {
 			title: "Oslo Pride"
 		}),
 		new FaviconsWebpackPlugin("./favicon.png"),
+		new SourceMapDevToolPlugin({
+			filename: "[name].[hash].js.map",
+			exclude: ["vendor"],
+			append: "//# sourceMappingURL=[url]"
+		}),
 		process.env.NODE_ENV === "production" &&
 			process.env.SANITY_PREVIEW !== "true" &&
 			new SentryWebpackPlugin({
 				include: ".",
-				ignoreFile: ".sentrycliignore",
 				ignore: ["node_modules", "webpack.config.js"],
 				configFile: "./sentry.properties"
 			}),
