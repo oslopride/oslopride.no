c8cfd9180bb2a40793fb640d1f270f2fd17cb040
luk-str <lstrociak@gmail.com>
2021-02-15 20:19:00 +0100

    Add optional button for scrolling to content

--- web/src/components/hero.tsx
+++ web/src/components/hero.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { css } from "@emotion/core";
 import AngledImage, { angleDirection } from "./angled-image";
+import theme from "../utils/theme";
 
 const hero = (height: string) => css`
 	min-height: 400px;
@@ -20,6 +21,27 @@ const image = (height: string) => css`
 	height: ${height};
 	min-height: 350px;
 	z-index: -1;
+`;
+
+const scrollButton = (height: string) => css`
+	display: block;
+	position: absolute;
+	top: calc(${height} - 1.5rem);
+	left: 50%;
+	transform: translateX(-50%);
+	height: 3rem;
+	width: 3rem;
+	color: #fff;
+	background: ${theme.color.main.pink};
+	border: 3px solid #fff;
+	border-radius: 50%;
+	cursor: pointer;
+	transition: background 0.3s;
+
+	:hover,
+	:focus {
+		background: ${theme.color.main.purple};
+	}
 `;
 
 const defaultContent = css`
@@ -50,6 +72,7 @@ type Props = {
 	overflow?: boolean;
 	className?: string;
 	textPosition?: "center" | "left";
+	displayScrollButton?: boolean;
 };
 
 const Hero: React.FC<Props> = props => {
@@ -63,11 +86,20 @@ const Hero: React.FC<Props> = props => {
 	} = props;
 
 	const contentRef = React.useRef<HTMLDivElement>(null);
+	const scrollButtonRef = React.useRef<HTMLButtonElement>(null);
 
 	const contentClass =
 		props.textPosition === "center"
 			? [defaultContent, centeredContent]
 			: defaultContent;
+
+	function scrollToContent(): void {
+		const buttonDistanceFromTop = scrollButtonRef.current?.offsetTop as number;
+		const buttonHeight = scrollButtonRef.current?.offsetHeight as number;
+		const scrollOffsetFromTop = buttonDistanceFromTop + buttonHeight;
+
+		window.scrollTo({ top: scrollOffsetFromTop, behavior: "smooth" });
+	}
 
 	return (
 		<div css={hero(height)}>
@@ -82,6 +114,19 @@ const Hero: React.FC<Props> = props => {
 					{children}
 				</div>
 			</div>
+
+			{props.displayScrollButton && (
+				<button
+					title="Scroll to main content"
+					ref={scrollButtonRef}
+					css={scrollButton(height)}
+					onClick={scrollToContent}
+				>
+					<span role="img" aria-label="Arrow pointing down">
+						↓
+					</span>
+				</button>
+			)}
 		</div>
 	);
 };
