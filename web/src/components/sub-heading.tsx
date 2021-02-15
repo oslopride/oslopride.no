cae74fdd291e18710f1e649fe8e8320e5abb965e
luk-str <lstrociak@gmail.com>
2021-02-15 11:24:42 +0100

    Remove line element from sub-heading component

--- web/src/components/sub-heading.tsx
+++ web/src/components/sub-heading.tsx
@@ -1,15 +1,4 @@
-import { css } from "@emotion/core";
 import styled from "@emotion/styled";
-
-import theme from "../utils/theme";
-
-const lineCss = css`
-	content: "";
-	width: 3em;
-	height: 2px;
-	background-color: ${theme.color.main.pink};
-	place-self: center;
-`;
 
 export type SubHeadingProps = { line: "left" | "right" | "both" | "line-only" };
 
@@ -19,32 +8,6 @@ const SubHeading = styled.span<SubHeadingProps>`
 	font-size: 0.85rem;
 	letter-spacing: 2px;
 	font-weight: 600;
-
-	${({ line }) =>
-		(line === "left" || line === "both") &&
-		css`
-			::before {
-				${lineCss}
-				margin-right: 1em;
-			}
-		`}
-
-	${({ line }) =>
-		(line === "right" || line === "both") &&
-		css`
-			::after {
-				${lineCss}
-				margin-left: 1em;
-			}
-		`}
-		
-	${({ line }) =>
-		line === "line-only" &&
-		css`
-			::after {
-				${lineCss}
-			}
-		`}
 `;
 
 export default SubHeading;
