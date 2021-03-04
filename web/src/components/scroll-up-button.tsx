7f1158dbb6c523b2a920f348fb49f688feb7e2b5
Roger Andersen <hexjelly@hexjelly.com>
2021-03-04 20:38:37 +0100

    wip

---
+++ web/src/components/scroll-up-button.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { css } from "@emotion/core";
+
+import theme from "../utils/theme";
+
+const scrollButton = css`
+	display: block;
+	height: 3rem;
+	width: 3rem;
+	color: #fff;
+	background: ${theme.color.main.pink};
+	border: 3px solid #fff;
+	border-radius: 50%;
+	cursor: pointer;
+	transition: background 0.3s;
+
+	:hover,
+	:focus {
+		background: ${theme.color.main.purple};
+	}
+`;
+
+const ScrollUpButton = () => {
+	const scrollButtonRef = React.useRef<HTMLButtonElement>(null);
+
+	function scrollToContent(): void {
+		// const buttonDistanceFromTop = scrollButtonRef.current?.offsetTop;
+		// const buttonHeight = scrollButtonRef.current?.offsetHeight;
+		// const scrollOffsetFromTop = buttonDistanceFromTop + buttonHeight;
+
+		window.scrollTo({ top: 0, behavior: "smooth" });
+	}
+
+	return (
+		<button
+			title="Bla opp til hovedinnholdet"
+			ref={scrollButtonRef}
+			css={scrollButton}
+			onClick={scrollToContent}
+		>
+			<span role="img" aria-label="Oppoverpil">
+				↑
+			</span>
+		</button>
+	);
+};
+
+export default ScrollUpButton;
--- web/src/pages/article.tsx
+++ web/src/pages/article.tsx
@@ -11,6 +11,7 @@ import SanityProtableText from "../components/sanity-portable-text";
 import Loading from "../components/loading";
 import NotFound from "./not-found";
 import Error from "./error";
+import ScrollUpButton from "../components/scroll-up-button";
 
 type Props = { slug?: string } & RouteComponentProps;
 
@@ -49,12 +50,40 @@ const date = css`
 	font-weight: 600;
 `;
 
+const nav = css`
+	display: flex;
+	justify-content: space-between;
+
+	h3 {
+		color: ${theme.color.text.grey};
+	}
+`;
+
 const Article: React.FC<Props> = props => {
 	const { slug } = props;
 
 	const { data: article, error } = useSWR<SanityArticle>(
 		`*[_type == "article" && slug.current == "${slug}"] | order(_updatedAt desc) [0]`
 	);
+
+	// get all articles in order to get next and prev from currently viewed article
+	const { data: articleList, error: _articleListError } = useSWR<
+		Array<SanityArticle>
+	>(`*[_type == "article"]{ _id, title, slug }  | order(_createdAt desc)`);
+
+	const indexOfCurrentArticle =
+		articleList?.findIndex(articleItem => articleItem._id === article?._id) ??
+		-1;
+
+	const nextArticle =
+		indexOfCurrentArticle >= 1
+			? articleList?.[indexOfCurrentArticle - 1]
+			: null;
+
+	const prevArticle =
+		indexOfCurrentArticle < (articleList?.length || 0) - 1
+			? articleList?.[indexOfCurrentArticle + 1]
+			: null;
 
 	if (error) return <Error error={JSON.stringify(error)} />;
 	if (article === undefined) return <Loading />;
@@ -79,6 +108,25 @@ const Article: React.FC<Props> = props => {
 			<div css={body}>
 				{article.body?.no && <SanityProtableText blocks={article.body.no} />}
 			</div>
+			<nav css={nav}>
+				{prevArticle && (
+					<div>
+						<h3>Forrige artikkel</h3>
+						<a href={`https://www.oslopride.no/a/${prevArticle.slug.current}`}>
+							{prevArticle.title.no}
+						</a>
+					</div>
+				)}
+				<ScrollUpButton />
+				{nextArticle && (
+					<div>
+						<h3>Neste artikkel</h3>
+						<a href={`https://www.oslopride.no/a/${nextArticle.slug.current}`}>
+							{nextArticle.title.no}
+						</a>
+					</div>
+				)}
+			</nav>
 
 			<Seo
 				openGraph={{
