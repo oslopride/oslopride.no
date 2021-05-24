ca07c09c49cccafc8df1b05e693ec99fb796b4fb
Boye Borg <6630430+boyeborg@users.noreply.github.com>
2021-05-24 11:35:31 +0200

    update simple events with new properties

--- sanity/schemas/simple-event.js
+++ sanity/schemas/simple-event.js
@@ -7,8 +7,28 @@ export default {
 	icon: MdEvent,
 	type: "document",
 	initialValue: {
-		official: true
+		official: true,
+		signLanguageInterpreted: false,
+		wheelchairFriendly: false,
+		liveStream: false
 	},
+	fieldsets: [
+		{
+			name: "accessibility",
+			title: "Accessibility",
+			options: { collapsible: true, collapsed: false }
+		},
+		{
+			name: "time",
+			title: "Time",
+			options: { collapsible: true, collapsed: false }
+		},
+		{
+			name: "location",
+			title: "Location",
+			options: { collapsible: true, collapsed: false }
+		}
+	],
 	fields: [
 		{
 			title: "Official Oslo Pride Event",
@@ -56,10 +76,26 @@ export default {
 			},
 			(lang, Rule) => (lang.isDefault ? Rule.required() : undefined)
 		),
+		localize(
+			{
+				title: "Blurb",
+				description:
+					"Short description used by previews, in search results and when sharing on social media",
+				name: "blurb",
+				type: "string"
+			},
+			(lang, Rule) =>
+				lang.isDefault
+					? Rule.required()
+							.min(5)
+							.max(280)
+					: Rule.min(5).max(280)
+		),
 		{
 			title: "Start time",
 			name: "startTime",
 			description: "When does the event begin?",
+			fieldset: "time",
 			type: "datetime",
 			validation: Rule => Rule.required()
 		},
@@ -67,45 +103,72 @@ export default {
 			title: "End time",
 			name: "endTime",
 			description: "When does the event end?",
+			fieldset: "time",
 			type: "datetime"
 		},
 		{
-			title: "Price",
-			name: "price",
-			description: "Does the event cost anything?",
-			type: "string"
-		},
-		{
-			title: "Event Link",
-			name: "eventLink",
-			type: "url",
+			name: "category",
+			type: "string",
+			options: {
+				list: [
+					{ title: "Concert", value: "concert" },
+					{ title: "Debate", value: "debate" },
+					{ title: "Talk", value: "talk" },
+					{ title: "Party", value: "party" },
+					{ title: "Other", value: "other" }
+				]
+			},
 			validation: Rule => Rule.required()
 		},
 		{
-			title: "Organizer",
-			name: "organizer",
-			type: "string"
-		},
-		{
-			title: "Contact",
-			name: "contact",
-			description: "For internal use, but will be publicly accessible",
-			type: "object",
-			fields: [
-				{
-					name: "name",
-					type: "string"
-				},
-				{
-					name: "email",
-					type: "string"
-				}
-			]
-		},
-		{
-			name: "documentOwner",
+			name: "arena",
 			type: "string",
-			hidden: true
+			options: {
+				list: [
+					{ title: "Pride Park", value: "park" },
+					{ title: "Pride House", value: "house" },
+					{ title: "Pride Parade", value: "parade" },
+					{ title: "External", value: "external" },
+					{ title: "Other", value: "other" }
+				]
+			},
+			fieldset: "location",
+			validation: Rule => Rule.required()
+		},
+		{
+			name: "address",
+			type: "string",
+			fieldset: "location"
+		},
+		{
+			name: "signLanguageInterpreted",
+			type: "boolean",
+			fieldset: "accessibility",
+			validation: Rule => Rule.required()
+		},
+		{
+			name: "wheelchairFriendly",
+			type: "boolean",
+			fieldset: "accessibility",
+			validation: Rule => Rule.required()
+		},
+		{
+			name: "liveStream",
+			description:
+				"This indicates that the event will be streamed on the official Oslo Pride live stream",
+			type: "boolean",
+			fieldset: "accessibility",
+			validation: Rule => Rule.required()
+		},
+		{
+			name: "eventLink",
+			description: "URL to the event website (e.g. facebook, youtube etc)",
+			type: "url"
+		},
+		{
+			name: "organizer",
+			type: "string",
+			validation: Rule => Rule.required()
 		}
 	],
 	preview: {
