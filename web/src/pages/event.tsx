8fa178233d5890e4a0625c2d52e1738a8765fc67
Boye Borg <6630430+boyeborg@users.noreply.github.com>
2021-06-06 12:13:57 +0200

    remove default organizer for official events

--- web/src/pages/event.tsx
+++ web/src/pages/event.tsx
@@ -203,7 +203,7 @@ const Event: React.FC<Props> = props => {
 				<ul className="details">
 					<li>
 						<span>Arrangør</span>
-						<span>{event.official ? "Oslo Pride" : event.organizer}</span>
+						<span>{event.organizer}</span>
 					</li>
 					<li>
 						<span>Dato og tid</span>
