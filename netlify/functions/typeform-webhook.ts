a70c6b0b36499daf34eca12847709c4358822b74
Roger Andersen <hexjelly@hexjelly.com>
2021-10-31 12:18:17 +0100

    🤔

---
+++ netlify/functions/typeform-webhook.ts
@@ -0,0 +1,12 @@
+import { Handler } from "@netlify/functions";
+
+export const handler: Handler = (event, context, callback) => {
+  const body = JSON.parse(event.body || "")?.payload;
+
+  console.log(body);
+
+  return callback(null, {
+    statusCode: 200,
+    body: "Beep, boop, you just got serverless."
+  });
+};
