c0a4e3c515f5583595c02ee96ae6f745c1e35a50
Roger Andersen <hexjelly@hexjelly.com>
2021-11-28 18:02:14 +0100

    nvm busboy

--- netlify/functions/submission-created.ts
+++ netlify/functions/submission-created.ts
@@ -1,51 +1,5 @@
 import { Handler } from "@netlify/functions";
 import sanityClient from "@sanity/client";
-import Busboy from "busboy";
-
-function parseMultipartForm(event: any): Promise<Record<any, any>> {
-  return new Promise(resolve => {
-    // we'll store all form fields inside of this
-    const fields: Record<any, any> = {};
-
-    // let's instantiate our busboy instance!
-    const busboy = new Busboy({
-      // it uses request headers
-      // to extract the form boundary value (the ----WebKitFormBoundary thing)
-      headers: event.headers
-    });
-
-    // before parsing anything, we need to set up some handlers.
-    // whenever busboy comes across a file ...
-    busboy.on(
-      "file",
-      (fieldname, filestream, filename, transferEncoding, mimeType) => {
-        // ... we take a look at the file's data ...
-        filestream.on("data", data => {
-          // ... and write the file's name, type and content into `fields`.
-          fields[fieldname] = {
-            filename,
-            type: mimeType,
-            content: data
-          };
-        });
-      }
-    );
-
-    // whenever busboy comes across a normal field ...
-    busboy.on("field", (fieldName, value) => {
-      // ... we write its value into `fields`.
-      fields[fieldName] = value;
-    });
-
-    // once busboy is finished, we resolve the promise with the resulted fields.
-    busboy.on("finish", () => {
-      resolve(fields);
-    });
-
-    // now that all handlers are set up, we can finally start processing our request!
-    busboy.write(event.body);
-  });
-}
 
 const SANITY_TOKEN = process.env.SK2022_SUBMISSION_SANITY_TOKEN;
 const PROJECT_ID = "2ger3rla";
@@ -64,18 +18,9 @@ export const handler: Handler = async (event, context) => {
   const body = JSON.parse(event.body || "")?.payload;
   console.log(body);
 
-  const fields = await parseMultipartForm(event);
-  console.log(fields);
-
-  if (fields.image) {
-    // const assetDocument = client.assets
-    // .upload('file', file)
-    // .then((document) => {
-    //   console.log('The file was uploaded!', document)
-    // })
-    // .catch((error) => {
-    //   console.error('Upload failed:', error.message)
-    // })
+  if (body.data.image) {
+    const assetDocument = await client.assets.upload("image", body.data.image);
+    console.log(assetDocument);
   }
 
   // const doc = {
