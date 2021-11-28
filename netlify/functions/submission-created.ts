ee01aea6483e61f9952c11545400bc1576e2220b
Roger Andersen <hexjelly@hexjelly.com>
2021-11-28 17:25:43 +0100

    see what we get

--- netlify/functions/submission-created.ts
+++ netlify/functions/submission-created.ts
@@ -13,10 +13,23 @@ const client = sanityClient({
 });
 
 export const handler: Handler = async (event, context) => {
-  console.log(context);
-  console.log(DATASET);
-  const body = JSON.parse(event.body);
+  // console.log(context);
+  // console.log(DATASET);
+  const body = JSON.parse(event.body || "")?.payload;
   console.log(body);
+
+  if (body.data.image) {
+    console.log(body.data.image.content);
+    console.log(body.data.image.toString());
+    // const assetDocument = client.assets
+    // .upload('file', file)
+    // .then((document) => {
+    //   console.log('The file was uploaded!', document)
+    // })
+    // .catch((error) => {
+    //   console.error('Upload failed:', error.message)
+    // })
+  }
 
   // const doc = {
   //   _type: "externalEventRequest",
